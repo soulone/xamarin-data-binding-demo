cd2fbf7da76722a3de0a2feec08c7513a1b81e4c
soulone <ascf24032@gmail.com>
2019-10-07 22:57:05 -0500

    First Commit

---
+++ ViewToViewDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Application
+{
+    public class ViewToViewDemo : ContentPage
+    {
+        public ViewToViewDemo()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
